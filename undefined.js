bb580d843844dae9a07f33ca51040540790035b0
sayeedreza99 <sayeedreza99.com>
2020-08-08 04:42:10 +0600

    null versus undefined cleared for interview

---
+++ undefined.js
@@ -0,0 +1,10 @@
+// let pakhi;
+// function add(num1, num2) {
+//     console.log(num1 + num2);
+//     return 
+// }
+// const result = add(23, 54);
+// console.log(result);
+
+let fun = undefined;
+console.log(fun);
