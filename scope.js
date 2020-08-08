e152accd3d8eb341e41486d650e606b4ce23d6e8
sayeedreza99 <sayeedreza99.com>
2020-08-09 01:30:56 +0600

    scope-var-let-const

---
+++ scope.js
@@ -0,0 +1,17 @@
+let output = 56;
+function sum(first, second) {
+    output = first + second + output;
+    if (output > 50) {
+        var mood = 'happy'
+        mood = "funcy";
+
+
+    }
+
+    var day = 'friday';
+    console.log(day);
+    return output
+}
+
+const result = sum(35, 5);
+console.log(output);
